52208dd417540b6eb6b4ed491621c53460b2aee1
mattkumar <mattkumar@gmail.com>
2022-05-05 21:23:20 -0400

    inittttttttttttttttttttttt

---
+++ custom_fullpage/app.R
@@ -0,0 +1,89 @@
+# setup 
+# setwd("custom_fullpage")
+source("setup.R")
+
+# ui
+ui <- fullPage(
+  
+  # settings
+  includeCSS("custom_css.css"),
+  opts = options,
+  
+  # menu
+  menu = c("Intro" = "intro",
+           "Section 1" = "s1",
+           "Section 2" = "s2",
+           "Section 3" = "s3",
+           "Section 4" = "s4",
+           "About" = "about"),
+  # intro
+  fullSection(
+    center = TRUE,
+    menu = "intro",
+    tags$h1("This is the Intro page")
+  ),
+  
+  # section 1
+  fullSection(
+    center = TRUE,
+    menu = "s1",
+    tags$h1("This is Section 1")
+  ),
+  
+  # section 2
+  fullSection(
+    menu = "s2",
+    fullSlide(
+      
+        center = TRUE,
+        h3("This is Section 2"),
+        h3("Slide 1")
+     
+    ),
+    fullSlide(
+     
+        center = TRUE,
+        h3("This is Section 2"),
+        h3("Slide 2")
+      
+    ),
+    fullSlide(
+      
+        center = TRUE,
+        h3("This is Section 2"),
+        h3("Slide 3")
+      
+    )
+  ),
+  
+  # section 3
+  fullSection(
+    center = TRUE,
+    menu = "s3",
+    tags$h1("This is Section 3")
+  ),
+  
+  # section 4
+  fullSection(
+    center = TRUE,
+    menu = "s4",
+    tags$h1("This is Section 4")
+  ),
+  
+  # extra
+  fullSection(
+    menu = "about",
+    highchartOutput('chart1'))
+)
+
+# server
+server <- function(input, output){
+  
+  output$chart1 <- renderHighchart({
+    highcharts_demo()
+  })
+  
+}
+
+# run app
+shinyApp(ui, server)
---
+++ custom_fullpage/setup.R
@@ -0,0 +1,19 @@
+# Libs
+library(shiny)
+library(fullPage)
+
+# Options for full Page
+options <- list(
+  sectionsColor = c('#d11141',
+                    '#00b159', 
+                    '#00aedb', 
+                    '#f37735', 
+                    '#ffc425',
+                    '#fff'),
+  parallax = TRUE,
+  navigation = TRUE,
+  fadingEffect = TRUE
+)
+
+
+ 
